95a293665621ff93e3a50bc0059011cc7d5a0a35
Eric Ferraiuolo <eferraiuolo@gmail.com>
2013-10-26 01:37:26 -0400

    Add Yahoo copyright banner to src files

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+/*
+Copyright (c) 2013, Yahoo! Inc. All rights reserved.
+Copyrights licensed under the New BSD License.
+See the accompanying LICENSE file for terms.
+*/
+
 'use strict';
 
 var annotations = require('express-annotations'),
